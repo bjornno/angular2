31c97aa9e83fdbbbc5043f77292cca44a6e7d83f
Bjørn Nordlund <bnordlun@cisco.com>
2016-08-24 13:57:28 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+npm install
+npm start
+
+todo: https://angular.io/docs/ts/latest/tutorial/toh-pt6.html
---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }  from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
+import { routing }        from './app.routing';
+
+@NgModule({
+  imports: [ 
+  	BrowserModule, 
+  	FormsModule,
+  	routing
+	],
+  declarations: [ 
+  	AppComponent, 
+  	HeroesComponent, 
+  	DashboardComponent, 
+  	HeroDetailComponent 
+	],
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
